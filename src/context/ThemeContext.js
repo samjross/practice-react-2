d89d8405f4ab9371b431ae4ef9caa67eb34084af
Sam <sjoshuaross@yahoo.com>
2020-10-09 10:09:31 +0100

    storing theme values in localStorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -25,8 +25,14 @@ export class ThemeContextProvider extends React.Component {
     constructor(props) {
         super(props);
 
+        let defaultTheme = themes.dark;
+        const localStorageTheme = localStorage.getItem('ThemeContext.theme');
+        if (localStorageTheme) {
+            defaultTheme = JSON.parse(localStorageTheme);
+        }
+
         this.state = {
-            theme: themes.dark,
+            theme: defaultTheme,
             toggleTheme: this.toggleTheme.bind(this),
             setTheme: this.setTheme.bind(this)
         };
@@ -40,7 +46,7 @@ export class ThemeContextProvider extends React.Component {
                 state.theme === themes.dark
                     ? themes.light
                     : themes.dark,
-        }));
+        }), this.newStateCallback);
     }
 
     setTheme(foreground, background) {
@@ -48,7 +54,12 @@ export class ThemeContextProvider extends React.Component {
         newState.theme = {
             foreground, background
         }
-        this.setState(newState);
+        this.setState(newState, this.newStateCallback);
+    }
+
+    newStateCallback() {
+        const theme = this.state.theme;
+        localStorage.setItem('ThemeContext.theme', JSON.stringify(theme));
     }
 
     render() {
